390547d51664f57b10009ed17f2b512316f61fa1
Mr <lkarpik@wp.pl>
2018-11-10 09:25:42 +0100

    done: interface html, database, PDO class

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+//DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "forum");
+
+define("SITE_TITLE", "Welcome To PHP Forum!");
+
+//Paths
+define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/forum/');
